8ab8665bde7de7669267fcab148e83106e1ed2c9
ae20006shibaura <131850849+ae20006shibaura@users.noreply.github.com>
2023-06-23 16:09:50 +0900

    ae

---
+++ picture_project/apps/pictapp/views.py
@@ -0,0 +1,55 @@
+
+""" 識別名をpictappにしてBlueprintオブジェクトを生成
+
+    ・テンプレートフォルダーは同じディレクトリの'templates_pict'
+    ・staticフォルダーは同じディレクトリの'static_pict'
+"""
+from flask import Blueprint
+
+pictapp = Blueprint(
+    'pictapp',
+    __name__,
+    template_folder='templates_pict',
+    static_folder='static_pict',
+    )
+
+"""pictappのトップページのルーティングとビューの定義
+"""
+from flask import render_template
+from flask_login import login_required # login_required
+from apps.app import db
+
+# ログイン必須にする
+@pictapp.route('/', methods=['GET', 'POST'])
+@login_required
+def index():
+    # top.htmlをレンダリングする
+    return render_template('top.html')
+
+"""ログアウトのルーティングとビューの定義
+"""
+from flask_login import logout_user
+from flask import render_template, url_for, redirect
+
+@pictapp.route('/logout')
+@login_required
+def logout():
+    # flask_loginのlogout_user()関数でログイン中のユーザーを
+    # ログアウトさせる
+    logout_user()
+    # ログイン画面のindexビューにリダイレクト
+    return redirect(url_for('authapp.index'))
+from sqlalchemy import select
+from flask import request
+from apps import models as modelpict
+@pictapp.route('/user-list/,int:user_id>')
+@login_required
+def user_list(user_id):
+    stmt=select(
+        modelpict.UserPicture).filter_by(user_id).order_by(modelpict.UserPicture.create_at.desc())
+    userlist=db.session.excute(stmt).scalars().all()
+    return render_template(
+        'userlist.html',userlist=userlist
+    )
+
+
